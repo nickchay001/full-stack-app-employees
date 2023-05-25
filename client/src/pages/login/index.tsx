05cb07ca08066a3552c7425a52a0f464ccf41a1a
Nick <119807798+nickchay001@users.noreply.github.com>
2023-05-25 11:03:22 +0300

    RTK Query connection

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,17 +1,41 @@
+import { useState } from 'react'
 import { Layout } from '../../components/layout'
 import { Card, Form, Row, Space, Typography } from 'antd'
 import { CustomeInput } from '../../components/custome-input'
 import { PasswordInput } from '../../components/passworrd-input'
 import { CustomeButton } from '../../components/custome-button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Paths } from '../../paths'
+import { UserData, useLoginMutation } from '../../app/services/auth'
+import { isErrorWhithMessage } from '../../utils/is-error-with-message'
+import { ErrorMessage } from '../../components/error-message'
 
 export const Login = () => {
+    const navigate = useNavigate()
+    const [loginUser, loginUserResult] = useLoginMutation()
+    const [error, setError] = useState('')
+
+    const login = async (data: UserData) => {
+        try {
+            await loginUser(data).unwrap()
+            
+            navigate("/")
+        } catch (err) {
+            const maybeError = isErrorWhithMessage(err)
+
+            if (maybeError) {
+                setError(err.data.message)
+            } else {
+                setError('Неизвестная ошибка')
+            }
+        }
+    }
+
     return (
         <Layout>
             <Row align="middle" justify="center">
                 <Card title="Войдите" style={{ width: "30rem" }}>
-                    <Form onFinish={() => null}>
+                    <Form onFinish={login}>
                         <CustomeInput type="email" name="email" placeholder="Email" />
                         <PasswordInput name="password" placeholder="Пароль" />
                         <CustomeButton type="primary" htmlType="submit">
@@ -22,6 +46,7 @@ export const Login = () => {
                         <Typography.Text>
                             Нет Аккаунта ? <Link to={Paths.register}>Зарегистрируйтесь</Link>
                         </Typography.Text>
+                        <ErrorMessage message={error} />
                     </Space>
                 </Card>
             </Row>
