3d8b2700b276e96c9293e7a9c5b23e619334d0e2
Nick <119807798+nickchay001@users.noreply.github.com>
2023-05-26 13:07:11 +0300

    registration

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,15 +3,45 @@ import { Card, Form, Row, Space, Typography } from 'antd'
 import { CustomeInput } from '../../components/custome-input'
 import { PasswordInput } from '../../components/passworrd-input'
 import { CustomeButton } from '../../components/custome-button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Paths } from '../../paths'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../features/auth/authSlice'
+import { useState } from 'react'
+import { useRegisterMutation } from '../../app/services/auth'
+import { User } from '@prisma/client'
+import { isErrorWhithMessage } from '../../utils/is-error-with-message'
+import { ErrorMessage } from '../../components/error-message'
+
+type RegisterData = Omit<User, "id"> & { confirmPassword: string }
+
 
 export const Register = () => {
+  const navigate = useNavigate()
+  const user = useSelector(selectUser)
+  const [error, setError] = useState('')
+  const [registerUser] = useRegisterMutation()
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap()
+      navigate('/')
+    } catch (error) {
+      const maybeError = isErrorWhithMessage(error)
+
+      if (maybeError) {
+        setError(error.data.message)
+      } else {
+        setError('Неизвестная ошибка')
+      }
+    }
+  }
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Зарегестрируйтесь" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <CustomeInput name="name" placeholder="Имя" />
             <CustomeInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
@@ -24,6 +54,7 @@ export const Register = () => {
             <Typography.Text>
               Уже зарегемтрированы ? <Link to={Paths.login}>Войдите</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
